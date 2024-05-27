7680410d70c0436fe36a7fd3bd14f7f5733f90b4
Your Name <asmitastha2001@gmail.com>
2024-05-27 15:19:43 +0545

    header added

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Headers/Header";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="">
+        <Header />
+      </p>
+    </div>
+  );
+}
