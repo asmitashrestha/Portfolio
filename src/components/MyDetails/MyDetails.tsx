f1f219885757a4e839629a32013a27a8e03e1ac4
Your Name <asmitastha2001@gmail.com>
2024-06-03 19:36:18 +0545

    update: details sections

--- src/components/MyDetails/MyDetails.tsx
+++ src/components/MyDetails/MyDetails.tsx
@@ -10,14 +10,14 @@ export default function MyDetails() {
         <div className="font-semibold">2023-2024</div>
         <div className="">
           <h1 className="font-serif font-semibold">CodeClause</h1>
-          <p className="text-gray font-sans">Full Stack Developer</p>
+          <p className="text-gray font-sans">Web Developer</p>
         </div>
       </div>
       <div className="sans-serif flex space-x-7  text-white text-lg bg-darkgray rounded-2xl p-4 m-4 hover:border-2 hover:border-yellow">
         <div className="font-semibold">2023-2024</div>
         <div className="">
           <h1 className="font-serif font-semibold">Arbyte</h1>
-          <p className="text-gray font-sans">Full Stack Developer</p>
+          <p className="text-gray font-sans">Web Developer</p>
         </div>
       </div>
       <div className="sans-serif flex space-x-7  text-white text-lg bg-darkgray rounded-2xl p-4 m-4 hover:border-2 hover:border-yellow">
